4fa064f140ea3d799fb326f54c20adb6782d9aae
Dan Kwon <dankwon@Dans-Air-2.home>
2015-05-23 18:25:58 -0400

    handled '/' route for site

--- routes/site.js
+++ routes/site.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('site/home', {'title':'Home'});
+});
+
+
+
 router.get('/:page', function(req, res, next) {
 	var page = req.params.page;
 	res.render('site/'+page, {'title':page.toUpperCase()});
