e64a4d1b958b5ac52a39f85c7ccbed1778d7c951
Dan Kwon <dankwon@Dans-MacBook-Air-2.local>
2015-06-07 18:57:21 -0400

    set up search handler for routing searches through pubmed database

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,60 @@
 var express = require('express');
+var request = require('request');
+var xmlToJson = require('xml2js');
 var Profile = require('../models/Profile');
 var router = express.Router();
+
+
+function urlRequest(url, completion){
+	request.get(url, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var parseString = xmlToJson.parseString;
+			parseString(body, function (err, result) {
+				if (err){
+					console.log(err.message);
+					return;
+				}
+				 	
+			    console.log(result);
+				
+				if (completion != null)
+					completion(result);
+				
+				return;
+			});
+	    }
+	});
+	
+}
 
 /* GET users listing. */
 router.get('/:resource', function(req, res, next) {
 
 	var resource = req.params.resource;
+	if (resource == 'search'){
+		console.log('SEARCH: '+JSON.stringify(req.query));
+		
+		var baseUrl = 'http://www.ncbi.nlm.nih.gov/entrez/eutils/';
+		var url = baseUrl+'esearch.fcgi?db=pubmed&term='+req.query.term+'&usehistory=y&retmax=100';
+		var results = urlRequest(url, function(results){
+			var eSearchResult = results.eSearchResult;
+			var webEnv = eSearchResult.WebEnv;
+			console.log('WEB ENV: '+webEnv);
+			
+			var nextReq = baseUrl+'efetch.fcgi?db=Pubmed&retstart=0&retmax=100&usehistory=y&query_key=1&WebEnv='+webEnv+'&reldate=36500&retmode=xml';
+			urlRequest(nextReq, function(results){
+				res.setHeader('content-type', 'application/json');
+				var json = JSON.stringify({'confirmation':'sucess','results':results}, null, 2); // this makes the json 'pretty' by indenting it
+				res.send(json);
+				return;
+			});
+		});
+		
+		return;
+	}
+	
+	
+	
 	if (resource=='profiles'){
 		Profile.find(req.query, function(err, profiles) {
 			if (err){
