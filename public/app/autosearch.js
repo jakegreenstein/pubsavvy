7abc1b78a4a4433246ede03412e0e88aaa7da0a4
bkarpinos <bkarpinos@gmail.com>
2015-06-29 14:58:59 -0400

    add and delete from autoSearch Terms

--- public/app/autosearch.js
+++ public/app/autosearch.js
@@ -34,25 +34,28 @@ app.controller('AutoSearchController', ['$scope', '$http', function($scope, $htt
 
     $scope.addTerm = function(){
         console.log('NEWTERM: '+$scope.newTerm);
+        console.log('NEWTERM INDEX: '+ $scope.autoSearchValue['terms'].indexOf($scope.newTerm));
+        if($scope.autoSearchValue['terms'].indexOf($scope.newTerm) != -1){
+            alert('Term already included in list');
+            return;
+        }
+
         $scope.autoSearchValue['terms'].push($scope.newTerm);
 
         var json = JSON.stringify($scope.autoSearchValue);
         console.log('UPDATE AUTOSEARCH: '+ json);
+        updateAutoSearch(json);
+    }
 
-        var url = '/api/autosearch/'+$scope.autoSearchID;
-        $http.put(url, json).success(function(data, status, headers, config) {
-            console.log(JSON.stringify(data));
-                        
-            var confirmation = data['confirmation'];
+    $scope.deleteTerm = function(term){
+        console.log('TERM TO DELETE: '+ term);
+        var index = $scope.autoSearchValue['terms'].indexOf(term);
+        if (index > -1)
+            $scope.autoSearchValue['terms'].splice(index, 1);
 
-            if (confirmation != 'success'){
-                alert(data['message']);
-                return;
-            }            
-            
-        }).error(function(data, status, headers, config) {
-            console.log("error", data, status, headers, config);
-        });
+        var json = JSON.stringify($scope.autoSearchValue);
+        console.log('UPDATE AUTOSEARCH: '+ json);
+        updateAutoSearch(json);
     }
 
     function getAutoSeachID(){
@@ -68,6 +71,23 @@ app.controller('AutoSearchController', ['$scope', '$http', function($scope, $htt
             $scope.autoSearchValue = data['autosearches'][0];
             $scope.autoSearchID = $scope.autoSearchValue['id'];
             console.log('AutoSearchID: '+$scope.autoSearchID)           
+            
+        }).error(function(data, status, headers, config) {
+            console.log("error", data, status, headers, config);
+        });
+    }
+
+    function updateAutoSearch(json){
+        var url = '/api/autosearch/'+$scope.autoSearchID;
+        $http.put(url, json).success(function(data, status, headers, config) {
+            console.log(JSON.stringify(data));
+                        
+            var confirmation = data['confirmation'];
+
+            if (confirmation != 'success'){
+                alert(data['message']);
+                return;
+            }            
             
         }).error(function(data, status, headers, config) {
             console.log("error", data, status, headers, config);
