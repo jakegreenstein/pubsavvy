ad99edb65239d5af0f48c48b9ff320da41501ba8
bkarpinos <bkarpinos@gmail.com>
2015-08-12 16:03:40 -0400

    remove theme from views and routes

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var sessions = require('client-sessions');
 var api = require('./routes/api');
 var site = require('./routes/site');
 var admin = require('./routes/admin');
-var theme = require('./routes/theme');
 
 
 var mongoose = require('mongoose');
@@ -56,7 +55,7 @@ app.use('/', site);
 app.use('/api', api);
 app.use('/site', site);
 app.use('/admin', admin);
-app.use('/theme', theme);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
