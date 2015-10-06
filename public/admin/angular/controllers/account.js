4746addb8ca715222e579b8f210e800f83c95a97
sarahsalder <sarahsalder@gmail.com>
2015-10-06 15:11:02 -0400

    persists images to back end

--- public/admin/angular/controllers/account.js
+++ public/admin/angular/controllers/account.js
@@ -42,7 +42,6 @@ app.controller('AccountController', ['$scope', '$http', '$upload', 'restService'
             
             $scope.profile = response['profile'];
             $scope.currentUser.loggedIn = 'yes';
-            console.log($scope.profile);
         });
     }
 
@@ -115,64 +114,12 @@ app.controller('AccountController', ['$scope', '$http', '$upload', 'restService'
                 alert(error.message);
                 return;
             }            
-
+            
             var image = response['image'];
-            console.log(JSON.stringify(image));
-            $scope.profile.image = image.address;
+            $scope.profile.image = image.id;
+            $scope.update();
         });
     }
-
-
-	// $scope.onFileSelect = function(files, property, entity){
- //   		var url = 'http://media-service.appspot.com/api/upload';
- //        $http.get(url).success(function(data, status, headers, config){
- //            if(data['confirmation'] != 'success'){
- //                alert(data['message']);
- //                return;
- //            }
- //            var uploadString = data['upload'];
- //            uploadFiles(files, uploadString, property, entity);
- //        }).error(function(data, status, headers, config) {
- //            console.log("error", data, status, headers, config);
- //        });
- //    }
-
-
-    // function uploadFiles($files, uploadString, property, entity) { 
-    //     for (var i = 0; i < $files.length; i++) {
-    //         var file = $files[i];
-    //         $scope.upload = $upload.upload({
-    //             url: uploadString, //upload.php script, node.js route, or servlet url
-    //             method: 'POST',
-    //             // headers: {'header-key': 'header-value'},
-    //             // withCredentials: true,
-    //             data: {myObj: $scope.myModelObj},
-    //             file: file // or list of files: $files for html5 only
-    //             /* set the file formData name ('Content-Desposition'). Default is 'file' */
-    //             //fileFormDataName: myFile, //or a list of names for multiple files (html5).
-    //         }).progress(function(evt) {
-    //             console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-    //     }).success(function(data, status, headers, config){ // file is uploaded successfully           
-    //         //console.log(JSON.stringify(data));
-    //         var confirmation = data['confirmation'];
-            
-    //         if (confirmation != 'success'){
-    //             alert(data['message']);
-    //             return;
-    //         }
-            
-            // if(property=='image'){
-            //     var image = data['image'];
-            //     if(entity=='profile'){
-            //         $scope.profile['image'] = image['address'];
-            //     }
-            // }
-
-    //         console.log('profile: '+JSON.stringify($scope.profile));
-
-    //       });
-    //     }
-    // }
 
     $scope.update = function(){
         if($scope.newPassword != $scope.confirmPassword){
