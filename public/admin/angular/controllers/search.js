b08525fb2adad1bc456e8ad4297268115fa263e3
sarahsalder <sarahsalder@gmail.com>
2015-10-06 16:05:26 -0400

    cleans up search module

--- public/admin/angular/controllers/search.js
+++ public/admin/angular/controllers/search.js
@@ -1,25 +1,20 @@
 var app = angular.module('SearchModule', []);
 
-
 app.controller('SearchController', ['$scope', 'restService', 'generalService', function($scope, restService, generalService){
     $scope['generalService'] = generalService;
     $scope.terms = null;
     $scope.articles = null;
     $scope.results = new Array();
-    $scope.keys = null;
     $scope.loading = false;
 	
 	$scope.init = function(){
-		console.log('Search Controller: INIT');
         var requestObject = $scope.generalService.parseLocation('admin');
 
-        console.log('addres: '+JSON.stringify(requestObject));
         if( requestObject.params.term == null ) // no default search, ignore
             return;
 
         $scope.terms = requestObject.params.term;
         $scope.search(0);
-
 	}
 
     $scope.search = function(offset){
@@ -31,7 +26,6 @@ app.controller('SearchController', ['$scope', 'restService', 'generalService', f
 
         restService.query({resource:'search', term:$scope.terms.toString(), offset:offset, id:null}, function(response){
             $scope.loading = false;
-            console.log(JSON.stringify(response));
             if (response.confirmation != 'success') {
                 alert('Error: ' + response.message);
                 return;
@@ -41,9 +35,4 @@ app.controller('SearchController', ['$scope', 'restService', 'generalService', f
             $scope.count = response['count'];
             });
         }
-
-
-
-
-
 }]);
