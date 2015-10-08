edb5a9b26e2d750f40b9c793aa7fe729da348a6b
sarahsalder <sarahsalder@gmail.com>
2015-10-08 14:04:58 -0400

    revises site.js route to admin/home instead of site/home

--- routes/site.js
+++ routes/site.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('site/home', {'title':'Home'});
+	res.render('admin/home', {'title':'Home'});
 });
 
 
