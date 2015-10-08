08ab3bd4e663a2240bda70ad4a2f6a720281cfa6
sarahsalder <sarahsalder@gmail.com>
2015-10-08 14:23:25 -0400

    adds fetchfile to api.js

--- routes/api.js
+++ routes/api.js
@@ -114,4 +114,15 @@ router.put('/:resource/:id', function(req, res, next) {
 
 });
 
+
+var fetchFile = function(path){
+	return new Promise(function (resolve, reject){
+
+		fs.readFile(path, 'utf8', function (err, data) {
+			if (err) {reject(err); }
+			else { resolve(data); }
+		});
+	});
+}
+
 module.exports = router;
