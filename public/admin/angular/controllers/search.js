406a6829f6ecbc50c6b59402fac550b78bfd980c
sarahsalder <sarahsalder@gmail.com>
2015-10-26 17:09:05 -0400

    includes journal and authors in saved searches

--- public/admin/angular/controllers/search.js
+++ public/admin/angular/controllers/search.js
@@ -4,9 +4,15 @@ searchCtr.controller('SearchController', ['$scope', 'restService', 'generalServi
     $scope['generalService'] = generalService;
     $scope.terms = null;
     $scope.articles = null;
-    $scope.results = new Array();
+    
     $scope.loading = false;
     $scope.profile = null;
+    $scope.resultsVisible = false;
+    // scope variables for pagination
+    $scope.results = new Array();
+    $scope.currentPage = 1
+    $scope.numPerPage = 10
+
 	
 	$scope.init = function(){
         accountService.checkCurrentUser(function(response, error){
@@ -34,7 +40,7 @@ searchCtr.controller('SearchController', ['$scope', 'restService', 'generalServi
             alert("Please enter search terms");
             return;
         }
-
+        
         $scope.loading = true;
         restService.query({resource:'search', term:$scope.terms.toString(), offset:offset, id:null}, function(response){
             $scope.loading = false;
@@ -44,9 +50,21 @@ searchCtr.controller('SearchController', ['$scope', 'restService', 'generalServi
             }
 
             $scope.results = response['results'];
+
+            $scope.$watch('currentPage + numPerPage', function() {
+            var begin = (($scope.currentPage - 1) * $scope.numPerPage)
+            , end = begin + $scope.numPerPage;
+
+            $scope.results = $scope.results.slice(begin, end);
+            });
+
             $scope.count = response['count'];
+            $scope.resultsVisible = true;
         });
+
     }
+
+
 
     $scope.logout = function(){
       accountService.logout(function(response, error){
