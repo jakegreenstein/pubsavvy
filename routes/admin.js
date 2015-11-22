c4910932425c5d6cca614c18f2ba41506e3d2391
dankwon234 <dan.kwon234@gmail.com>
2015-11-22 15:58:04 -0500

    authorized users only for admin pages

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,47 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/:page', function(req, res, next) {
-	var page = req.params.page;
-	res.render('admin/'+page, {'title':page.toUpperCase()});
+
+	if (!req.session){
+		res.send({'confirmation':'fail', 'message':'User not logged in.'});
+		return;
+	}
+
+	if (!req.session.user){
+		res.send({'confirmation':'fail', 'message':'User not logged in.'});
+		return;
+	}
+
+	var userId = req.session.user;
+
+	Profile.findById(userId, function(err, profile){
+		if (err){
+			req.session.reset();
+			res.send({'confirmation':'fail', 'message':'Profile '+userId+' not found'});
+			return;
+		}
+		
+		if (profile == null){
+			res.send({'confirmation':'fail', 'message':'Profile '+userId+' not found'});
+			return;
+		}
+
+		var authorized = ['dianaframe@gmail.com', 'dianaframe@earthlink.net'];
+		if (authorized.indexOf(profile.email) == -1){
+			res.send({'confirmation':'fail', 'message':'Unauthorized'});
+			return;
+		}
+
+		var page = req.params.page;
+		res.render('admin/'+page, {'title':page.toUpperCase()});
+		return;
+	});
+
+
+
+
+
+
 });
 
 module.exports = router;
