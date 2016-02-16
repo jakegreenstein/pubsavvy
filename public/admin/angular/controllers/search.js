c967b0507001dddce3e014f3f0329501799e98c9
Jake Greenstein <jag28020@gmail.com>
2016-02-16 15:04:55 -0500

    Redesigned search page

--- public/admin/angular/controllers/search.js
+++ public/admin/angular/controllers/search.js
@@ -3,7 +3,11 @@ var searchCtr = angular.module('SearchModule', []);
 searchCtr.controller('SearchController', ['$scope', 'restService', 'generalService', 'accountService', function($scope, restService, generalService, accountService){
     $scope['generalService'] = generalService;
     $scope.terms = null;
-    $scope.articles = null;
+    $scope.section = 'search';
+    $scope.device = null;
+    $scope.articles = {};
+    $scope.relatedArticles = {};
+    $scope.loading = false;
     
     $scope.loading = false;
     $scope.profile = null;
@@ -12,6 +16,9 @@ searchCtr.controller('SearchController', ['$scope', 'restService', 'generalServi
     $scope.results = new Array();
     $scope.currentPage = 1
     $scope.numPerPage = 10
+    $scope.searchClass = 'active';
+    $scope.historyClass = '';
+    $scope.savedClass = '';
 
 	
 	$scope.init = function(){
@@ -23,6 +30,7 @@ searchCtr.controller('SearchController', ['$scope', 'restService', 'generalServi
             }
 
             $scope.profile = response.profile;
+            getDevices();
         });
 
         var requestObject = $scope.generalService.parseLocation('admin');
@@ -34,6 +42,69 @@ searchCtr.controller('SearchController', ['$scope', 'restService', 'generalServi
         $scope.search(0);
 
 	}
+
+    function getDevices(){
+        console.log('triggered get devices');
+         restService.query({resource:'device', profileId:$scope.profile.id}, function(response){
+            console.log(JSON.stringify(response));
+            if (response.confirmation != 'success') {
+                alert('Error: ' + response.message);
+                return;
+            }
+            $scope.device = response.devices[0];
+            console.log("DEVICE:" + JSON.stringify($scope.device));
+
+            if ($scope.device == null) 
+                return;
+
+            if ($scope.device.saved.length == 0)
+                return;
+            
+            var pmid = $scope.device.saved[0];
+            getArticle(pmid);
+        });
+    }
+
+    function getArticle(pmid){
+        restService.query({resource:'search', pmid:pmid}, function(response){
+            console.log(JSON.stringify(response));
+
+            if (response.confirmation == 'success') {
+                var article = response['article'][0];
+                $scope.articles[article['pmid']] = article;
+            }
+
+            var index = $scope.device.saved.indexOf(pmid);
+
+            if (index < $scope.device.saved.length-1)
+                getArticle($scope.device.saved[index+1]);
+            else {
+                relatedPmids = $scope.device.saved.slice(0,3).join();
+                restService.query({resource:'related', pmid:relatedPmids, limit:5}, function(response){
+                    function createRelatedArticles(article, index, array) {
+                      $scope.relatedArticles[article['pmid']] = article;
+                      console.log(article);
+                    }
+                    var results = response.results;
+                    results.forEach(createRelatedArticles);
+                }, function(error, headers){
+                    console.log('ERROR ! ! ! -- '+JSON.stringify(error));
+                });
+
+
+            }
+
+        }); 
+    }
+
+    $scope.removeArticle = function(pmidIndex){
+        var newPmidList = $scope.device.saved.splice(pmidIndex, 1);
+        restService.put({resource:'device', id:$scope.device.id}, $scope.device, function(response){
+            $scope.device = response.device;
+        }, function(error, headers){
+            console.log('ERROR ! ! ! -- '+JSON.stringify(error));
+        });
+    }    
 
     $scope.search = function(offset){
         if ($scope.terms==null || $scope.terms==""){
@@ -61,6 +132,27 @@ searchCtr.controller('SearchController', ['$scope', 'restService', 'generalServi
             $scope.count = response['count'];
             $scope.resultsVisible = true;
         });
+    }
+
+    $scope.redirect = function(key){
+        console.log("REDIRECT");
+        $scope.terms = key;
+        $scope.section = 'search';
+        document.getElementById('searchTab').click();
+        $scope.historyClass = '';
+        $scope.searchClass = 'active';
+
+        $scope.search(0);
+    }
+
+    $scope.updateSection = function(newSection){
+        $scope.section = newSection;
+
+            if (newSection == "account-information"){
+                $('.recommended-articles').hide();
+            }
+            else 
+                $('.recommended-articles').show();
 
     }
 
